893f850036fc48456608f6bb7b525dcf3a6e9c59
LeeGwangSeock <xlwpdldhk@gmail.com>
2022-03-09 18:22:23 +0900

    초기 커밋.

---
+++ TabBarCustom/TabBarCustom/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using TabBarCustom.ViewModels;
+using TabBarCustom.Views;
+using Xamarin.Forms;
+
+namespace TabBarCustom
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ TabBarCustom/TabBarCustom/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using TabBarCustom.ViewModels;
+using Xamarin.Forms;
+
+namespace TabBarCustom.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
